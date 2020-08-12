a87c6724a4f7015eb8cee0b20d4dda1282d1668c
Aniketh Maddipati <aniketh@anikeths-mbp.lan>
2020-08-12 17:00:39 -0500

    basic querying works

--- functions/index.js
+++ functions/index.js
@@ -5,23 +5,21 @@ const cors = require('cors');
 const bodyParser = require('body-parser')
 const healthRoutes = require('./health');
 const readRoutes = require('./read');
-const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(cors({ origin: true }))
+app.use('/', readRoutes, healthRoutes)
 
+// not as clean, but a better endpoint to consume
+// const api = functions.https.onRequest((request, response) => {
+//   if (!request.path) {
+//     request.url = `/${request.url}` // prepend '/' to keep query params if any
+//   }
+//   return app(request, response)
+// });
 
-
-app.use('/', healthRoutes, readRoutes);
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  });
-
-
-const api = functions.https.onRequest(app)
+const api = functions.https.onRequest(app);
 
 module.exports = {
-    api
-};
-
+  api
+}
